51419b8d3be16f79a40e65d1f9bdbb18f8f21424
haisheng-lin <linhaicheng@xiao100.com>
2020-02-26 21:02:43 +0800

    feat: add recommend list page

---
+++ src/pages/Recommend/index.tsx
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from 'react';
+import { RouteComponentProps } from 'react-router-dom';
+
+import SongUsecase from 'shared/domain/song';
+import { FulfilledRecommendSong } from 'shared/domain/song/typings';
+
+import styles from './index.module.scss';
+
+const Recommend: React.FC<RouteComponentProps> = props => {
+  const [recommendList, setRecommendList] = useState<FulfilledRecommendSong[]>(
+    []
+  );
+
+  const getRecommendList = async () => {
+    try {
+      const result = await SongUsecase.getRecommendList();
+      setRecommendList(result);
+    } catch (e) {}
+  };
+
+  useEffect(() => {
+    getRecommendList();
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.listTitle}>热门歌单推荐</h1>
+        <ul>
+          {recommendList.map(item => (
+            <li key={item.songId} className={styles.item}>
+              <div className={styles.icon}>
+                <img
+                  src={item.smallSongPic}
+                  width="60"
+                  height="60"
+                  alt={item.songName}
+                />
+              </div>
+              <div className={styles.text}>
+                <h2 className={styles.name}>{item.singerName}</h2>
+                <span className={styles.desc}>{item.songName}</span>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Recommend;
