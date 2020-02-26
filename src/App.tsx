6b697dabeef0119d188fc1aad87bc8e06db3dee7
haisheng-lin <linhaicheng@xiao100.com>
2020-02-26 15:36:33 +0800

    feat: add assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
 
-import './App.css';
+import Header from 'shared/components/Header';
+import Tabs from 'shared/components/Tabs';
+import Routes from 'pages/Routes';
+
+import { LOCAL_PATHS } from 'shared/constants';
+
+import 'assets/styles/index.scss';
 
 const App: React.FC = () => {
-  return <div>Hello music-mweb</div>;
+  const tabs = [
+    { label: '推荐', path: LOCAL_PATHS.recommend.routePath },
+    { label: '歌手', path: LOCAL_PATHS.singer.routePath },
+    { label: '排行', path: LOCAL_PATHS.rank.routePath },
+    { label: '搜索', path: LOCAL_PATHS.search.routePath }
+  ];
+
+  return (
+    <>
+      <Header />
+      <Router>
+        <Tabs tabs={tabs} />
+        <Switch>
+          <Routes />
+        </Switch>
+      </Router>
+    </>
+  );
 };
 
 export default App;
